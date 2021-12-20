116b86c9572f8a57037ca76d93d7c8c0ba30e2f5
Astone Tuhame <astonetuhame@gmail.com>
2021-12-21 00:53:28 +0300

    Update FilterTodos.vue

--- src/components/FilterTodos.vue
+++ src/components/FilterTodos.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     Filter Todos:
-    <select name="" id="">
+    <select @change="filterTodos($event)">
       <option value="200">200</option>
       <option value="100">100</option>
       <option value="50">50</option>
@@ -13,8 +13,11 @@
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
   name: "FilterTodos",
+  methods: mapActions(["filterTodos"]),
 };
 </script>
 
