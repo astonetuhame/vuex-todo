aee132f4dd3152682d111ffe256f072770a4e70c
Astone Tuhame <astonetuhame@gmail.com>
2021-12-20 21:25:28 +0300

    Create Todos.vue

---
+++ src/components/Todos.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h3>Todos</h3>
+        <div class="todos">
+            <div v-for="(todo, index) in allTodos" :key="index" class="todo">
+                {{  todo.title }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+    name: "Todos",
+    computed: mapGetters(['allTodos'])
+}
+
+</script>
+
+
+<style>
+
+
+</style>
