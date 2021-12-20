dda89bb79aadbb4a9f1007917d1116dfe5ec9318
Astone Tuhame <astonetuhame@gmail.com>
2021-12-21 00:07:53 +0300

    Update todos.js

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,25 +1,45 @@
 import axios from "axios";
 
 const state = {
-  todos: [
-    {
-      id: 1,
-      title: "Todo One",
-    },
-    {
-      id: 2,
-      title: "Todo Two",
-    },
-  ],
+  todos: [],
 };
 
 const getters = {
   allTodos: (state) => state.todos,
 };
 
-const actions = {};
+const actions = {
+  async fetchTodos({ commit }) {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/todos"
+    );
 
-const mutations = {};
+    // console.log(response.data);
+    commit("setTodos", response.data);
+  },
+
+  async addTodo({ commit }, title) {
+    const response = await axios.post(
+      "https://jsonplaceholder.typicode.com/todos",
+      { title, completed: false }
+    );
+
+    commit("newTodo", response.data);
+  },
+
+  async deleteTodo({ commit }, id) {
+    await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
+
+    commit("removeTodo", id);
+  },
+};
+
+const mutations = {
+  setTodos: (state, todos) => (state.todos = todos),
+  newTodo: (state, todo) => state.todos.unshift(todo),
+  removeTodo: (state, id) =>
+    (state.todos = state.todos.filter((todo) => todo.id !== id)),
+};
 
 export default {
   state,
